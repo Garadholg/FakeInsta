a6d5b48a5789ab828647a6ebad303a3307005cb6
amaurov <amaurov@racunarstvo.hr>
2022-06-04 16:56:44 +0200

    implemented dynamical creation of hashtag textviews

--- app/src/main/java/com/amaurov/fakeinsta/adapters/PostsRecyclerViewAdapter.kt
+++ app/src/main/java/com/amaurov/fakeinsta/adapters/PostsRecyclerViewAdapter.kt
@@ -1,12 +1,19 @@
 package com.amaurov.fakeinsta.adapters
 
+import android.content.Context
+import android.graphics.Color
 import android.graphics.Typeface
+import android.text.Layout
 import android.text.SpannableString
 import android.text.SpannableStringBuilder
 import android.text.Spanned
 import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.compose.ui.unit.dp
+import androidx.core.view.marginLeft
 import androidx.recyclerview.widget.RecyclerView
 import com.amaurov.fakeinsta.databinding.PostItemBinding
 import com.amaurov.fakeinsta.viewmodels.PostViewModel
@@ -18,7 +25,7 @@ class PostsRecyclerViewAdapter(private val postList: List<PostViewModel>) : Recy
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         _binding = PostItemBinding.inflate(layoutInflater, parent, false)
-        return ViewHolder(binding)
+        return ViewHolder(parent.context, binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -29,12 +36,12 @@ class PostsRecyclerViewAdapter(private val postList: List<PostViewModel>) : Recy
         return postList.size
     }
 
-    inner class ViewHolder(private val binding: PostItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(private val context : Context, private val binding: PostItemBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(holder: ViewHolder, post: PostViewModel) {
             binding.username.text = post.username
             binding.caption.text = createCaptionText(post.username, post.caption)
 
-            createHashtags()
+            createHashtags(post.hashtags)
         }
 
         private fun createCaptionText(username: String, caption: String) : SpannableStringBuilder {
@@ -47,8 +54,23 @@ class PostsRecyclerViewAdapter(private val postList: List<PostViewModel>) : Recy
             return ssb
         }
 
-        private fun createHashtags() {
+        private fun createHashtags(hashtags: List<String>) {
+            for (hashtag in hashtags) {
+                var hashtagText = TextView(context)
+                hashtagText.text = hashtag
+                hashtagText.setTextColor(Color.BLUE)
 
+                if (hashtags.indexOf(hashtag) != 0) {
+                    var layoutParams = LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                    )
+                    layoutParams.setMargins(5, 0, 0, 0)
+                    hashtagText.layoutParams = layoutParams
+                }
+
+                binding.hashtagContainer.addView(hashtagText);
+            }
         }
     }
 }
